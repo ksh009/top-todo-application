33a082877837c38178091884f33c0ffe29345403
Kyle <khughessean01@gmail.com>
2023-05-10 14:02:59 +0200

    add project to TodoList

--- test.js
+++ test.js
@@ -1,51 +1,58 @@
 class Todo {
-  constructor(title, description, dueDate, priority) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.notes = [];
-    this.checklist = [];
-  }
+	constructor(title, description, dueDate, priority) {
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+		this.notes = [];
+		this.checklist = [];
+	}
 }
 
 class TodoList {
-  constructor() {
-    this.projects = [{ name: "Default", todos: [] }];
-    this.currentProjectIndex = 0;
-  }
+	constructor() {
+		this.projects = [{ name: 'Default', todos: [] }];
+		this.currentProjectIndex = 0;
+	}
 
-  addProject(name) {
-    const project = { name: name, todos: [] };
-    this.projects.push(project);
-  }
+	addProject(name) {
+		const project = { name: name, todos: [] };
+		this.projects.push(project);
+	}
 
-  removeProject(index) {
-    if (index === 0) {
-      throw new Error("Cannot delete default project");
-    }
-    this.projects.splice(index, 1);
-    if (this.currentProjectIndex >= index) {
-      this.currentProjectIndex -= 1;
-    }
-  }
+	removeProject(index) {
+		if (index === 0) {
+			throw new Error('Cannot delete default project');
+		}
+		this.projects.splice(index, 1);
+		// Run test here to check what happens after deletion
+		if (this.currentProjectIndex >= index) {
+			this.currentProjectIndex -= 1;
+		}
+	}
 
-  getCurrentProject() {
-    return this.projects[this.currentProjectIndex];
-  }
+	getCurrentProject() {
+		return this.projects[this.currentProjectIndex];
+	}
 
-  setCurrentProject(index) {
-    this.currentProjectIndex = index;
-  }
+	setCurrentProject(index) {
+		this.currentProjectIndex = index;
+	}
 
-  addTodoToCurrentProject(todo) {
-    const currentProject = this.getCurrentProject();
-    currentProject.todos.push(todo);
-  }
+	addTodoToCurrentProject(todo) {
+		const currentProject = this.getCurrentProject();
+		currentProject.todos.push(todo);
+	}
 
-  removeTodoFromCurrentProject(index) {
-    const currentProject = this.getCurrentProject();
-    currentProject.todos.splice(index, 1);
-  }
+	removeTodoFromCurrentProject(index) {
+		const currentProject = this.getCurrentProject();
+		currentProject.todos.splice(index, 1);
+	}
 }
 
+// Create a new TodoList instance
+const todoList = new TodoList();
+
+// Add a new project
+todoList.addProject('Work');
+console.log('todoList', todoList);
