e132b177c2f37f7079bb587028d0276828a81b27
Kyle <khughessean01@gmail.com>
2023-05-10 14:11:04 +0200

    Set current project index

--- test.js
+++ test.js
@@ -55,4 +55,23 @@ const todoList = new TodoList();
 
 // Add a new project
 todoList.addProject('Work');
-console.log('todoList', todoList);
+console.log('todoList After add', todoList);
+/* Output
+  todoList After add TodoList {
+    projects: [ { name: 'Default', todos: [] }, { name: 'Work', todos: [] } ],
+    currentProjectIndex: 0
+  }
+*/
+
+/* Modify further a little later when Todo is selected. From the nodelist then select the index based on onClick
+  - Could even prompt user after add if they could like to set as current project 
+*/
+// // Set the current project to the newly created project
+todoList.setCurrentProject(1);
+console.log('todoList After set project', todoList);
+/* Output
+  todoList After set project TodoList {
+    projects: [ { name: 'Default', todos: [] }, { name: 'Work', todos: [] } ],
+    currentProjectIndex: 1
+  }
+*/
