58ef9d5c47f5d59ce9383ae4fa0f0de9719e79de
Kyle <khughessean01@gmail.com>
2023-05-14 19:33:27 +0200

    Run index page once on load only

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import './style.css';
 // // state should probably be an object with getter and setter methods
 // Need to somehow check when the state changes so that the particular component can be rerendered
 
-export function index(state) {
+// Components
+// Initialized sections/components here
+
+// Selectors
+const content = document.getElementById('content');
+const headerContainer = document.createElement('header');
+const mainContainer = document.createElement('main');
+
+export function index() {
 	console.log('Check if webpack config working!!!');
-
-	// Components
-	// Initialized sections/components here
-
-	// Selectors
-	const content = document.getElementById('content');
-	const headerContainer = document.createElement('header');
-	const mainContainer = document.createElement('main');
 
 	// Add classes
 	headerContainer.classList.add('header-container');
@@ -27,4 +27,8 @@ export function index(state) {
 	content.appendChild(mainContainer);
 }
 
-index();
+// Ensure this only runs once on reload. Index will be triggered from other functions when state is updated. This is only fo initial load
+if (content.children.length === 1) {
+	// Later find a condition so that this function will only run once
+	index();
+}
