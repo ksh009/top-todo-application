bf401c8a15f39d57524d6bb90729e4a1302ed54f
Kyle <khughessean01@gmail.com>
2023-05-14 15:13:33 +0200

    Notes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+// Global state object will be created here
+// State object will be shared with each comp
+// Need to somehow check when the state changes so that the particular component can be rerendered
+
 console.log('Check if webpack config working!!!');
