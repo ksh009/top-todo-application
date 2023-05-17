fba2a9c82e45711cdc598a560954dd28579f424c
Kyle <khughessean01@gmail.com>
2023-05-17 12:13:03 +0200

    Prototype todo table - Table items not rendered dynamically yet

--- src/views/projectTodos.js
+++ src/views/projectTodos.js
@@ -37,46 +37,90 @@ export function createTodoList(appState) {
 	const todoList = document.createElement('div');
 	todoList.classList.add('todo-list');
 	todoList.innerHTML = `
-    <div class="todo-list">
     <div class="header date">
-        <span class="header-span">
-            <p>Date</p>
-            <hr class="project-hr" />
-        </span>
-    </div>
-    <div class="header description">
-        <span class="header-span">
-            <p>Title</p>
-            <hr class="project-hr" />
-        </span>
-    </div>
-    <div class="header priority">
-        <span class="header-span">
-            <p>Priority</p>
-            <hr class="project-hr" />
-        </span>
-    </div>
-    <div class="header completed">
-        <span class="header-span">
-            <p>Completed</p>
-            <hr class="project-hr" />
-        </span>
-    </div>
-    <div class="header update">
-        <span class="header-span">
-            <p>Update</p>
-            <hr class="project-hr" />
-        </span>
-    </div>
+						<span class="header-span">
+							<p>Date</p>
+							<hr class="project-hr" />
+						</span>
+					</div>
+					<div class="header description">
+						<span class="header-span">
+							<p>Title</p>
+							<hr class="project-hr" />
+						</span>
+					</div>
+					<div class="header priority">
+						<span class="header-span">
+							<p>Priority</p>
+							<hr class="project-hr" />
+						</span>
+					</div>
+					<div class="header completed">
+						<span class="header-span">
+							<p>Completed</p>
+							<hr class="project-hr" />
+						</span>
+					</div>
+					<div class="header update">
+						<span class="header-span">
+							<p>Update</p>
+							<hr class="project-hr" />
+						</span>
+					</div>
 
-    <div class="todo-item date">May 1, 2023</div>
-    <div class="todo-item description">Buy groceries</div>
-    <div id="priorityHigh" class="todo-item priority">High</div>
-    <div class="todo-item completed"><i class="fas fa-check"></i></div>
-    <div class="todo-item update">
-        <i title="Update todo" class="fa fa-eye"></i>
-        <i title="Delete todo" class="fa fa-trash"></i>
-    </div>
+					<div class="todo-item date">May 1, 2023</div>
+					<div class="todo-item description">Buy groceries</div>
+					<div id="priorityHigh" class="todo-item priority">High</div>
+					<div class="todo-item completed"><i class="fas fa-check"></i></div>
+					<div class="todo-item update">
+						<i title="Update todo" class="fa fa-eye"></i>
+						<i title="Delete todo" class="fa fa-trash"></i>
+					</div>
+
+					<div class="todo-item date">May 2, 2023</div>
+					<div class="todo-item description">Pay bills</div>
+					<div id="priorityMed" class="todo-item priority medium">Medium</div>
+					<div class="todo-item completed"><i class="fas fa-check"></i></div>
+					<div class="todo-item update">
+						<i title="Update todo" class="fa fa-eye"></i>
+						<i title="Delete todo" class="fa fa-trash"></i>
+					</div>
+
+					<div class="todo-item date">May 3, 2023</div>
+					<div class="todo-item description">Do laundry</div>
+					<div id="priorityLow" class="todo-item priority low">Low</div>
+					<div class="todo-item completed"><i class="fas fa-check"></i></div>
+					<div class="todo-item update">
+						<i title="Update todo" class="fa fa-eye"></i>
+						<i title="Delete todo" class="fa fa-trash"></i>
+					</div>
+
+					<div class="todo-item date">May 5, 2023</div>
+					<div class="todo-item description">Wash dogs</div>
+					<div id="priorityMed" class="todo-item priority medium">Medium</div>
+					<div class="todo-item completed"></div>
+					<div class="todo-item update">
+						<i title="Update todo" class="fa fa-eye"></i>
+						<i title="Delete todo" class="fa fa-trash"></i>
+					</div>
+
+					<div class="todo-item date">May 7, 2023</div>
+					<div class="todo-item description">Change tyres</div>
+					<div id="priorityHigh" class="todo-item priority">High</div>
+					<div class="todo-item completed"></div>
+					<div class="todo-item update">
+						<i title="Update todo" class="fa fa-eye"></i>
+						<i title="Delete todo" class="fa fa-trash"></i>
+					</div>
+
+					<div class="todo-item date">May 9, 2023</div>
+					<div class="todo-item description">Replant seeds</div>
+					<div id="priorityLow" class="todo-item priority low">Low</div>
+					<div class="todo-item completed"></div>
+					<div class="todo-item update">
+						<i title="Update todo" class="fa fa-eye"></i>
+						<i title="Delete todo" class="fa fa-trash"></i>
+					</div>
     `;
 
 	// // // create the new todo button element
